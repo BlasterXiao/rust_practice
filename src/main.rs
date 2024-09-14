5094b2d9ba3d949a0a167cfce71f09733f78cb74
BlasterXiao <28136107+BlasterXiao@users.noreply.github.com>
2024-09-14 13:19:13 +0800

    test code fun_calc

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
+fn fun_calc(x: i32, y: i32) -> i32 {
+    x + y
+}
+
 fn main() {
     println!("my first rust!");
-
     println!("commit branches");
+    let x = 1;
+    let y = 2;
+    println!("x + y = {}", fun_calc(x, y));
 }
